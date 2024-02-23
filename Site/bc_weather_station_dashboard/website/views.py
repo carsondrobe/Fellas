a57fe01266f88cbd9bce8e3575a977377fbe485e
Davis Franklin <davis_franklin@outlook.com>
2024-02-22 19:35:08 -0800

    created a login view

--- Site/bc_weather_station_dashboard/website/views.py
+++ Site/bc_weather_station_dashboard/website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html",{})
+
+def login_view(request):
+    return render(request, 'login.html')
