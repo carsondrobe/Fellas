1fe27b2204ff900c671d145163702b3ce15e0c3c
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-02-23 00:23:13 -0800

    Add models and register them in admin.py

--- Site/bc_weather_station_dashboard/website/admin.py
+++ Site/bc_weather_station_dashboard/website/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import User, WeatherStation
+from .models import User, WeatherStation, StationData, UserProfile, Dashboard, Alert, Feedback, ResponseFromAdmin
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(WeatherStation)
+admin.site.register(StationData)
+admin.site.register(Dashboard)
+admin.site.register(Alert)
+admin.site.register(Feedback)
+admin.site.register(ResponseFromAdmin)
+
+
