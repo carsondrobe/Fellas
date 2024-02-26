bd33233b547884127aafeda6ca9d4f03a7ede784
Davis Franklin <davis_franklin@outlook.com>
2024-02-25 16:56:40 -0800

    created a login view to authenticate users

--- Site/bc_weather_station_dashboard/website/views.py
+++ Site/bc_weather_station_dashboard/website/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
     return render(request, "home.html",{})
 
-def login_view(request):
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not username or not password:
+            return HttpResponse('Please fill in all fields', status=400)
+        
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # User is valid, log them in
+            login(request, user)
+            # Redirect to the home page
+            return redirect(reverse('home'))
+        else:
+            # Invalid username or password
+            return HttpResponse('Invalid username or password', status=400)
+    
     return render(request, 'login.html')
