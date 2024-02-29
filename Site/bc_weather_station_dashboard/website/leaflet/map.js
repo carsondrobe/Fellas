2f7fc023b231c8fe05fe0a3ae0ec22463e2425ae
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-02-28 19:09:31 -0800

    Developed a script to display all weather stations on the map.

--- Site/bc_weather_station_dashboard/website/leaflet/map.js
+++ Site/bc_weather_station_dashboard/website/leaflet/map.js
@@ -1,14 +1,46 @@
 // Initialize map to open with a zoomed out view of BC
-var map = L.map('map').setView([54.8, -125], 6);
+var map = L.map('map').setView([54.8, -125], 5);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Create random weather stations for now
-for(var i = 49; i <= 60; i += 2) {
-    for(var j = 121; j <= 130; j += 2) {
-        var marker = L.marker([i, -j]).addTo(map);
-        var popup = marker.bindPopup('<b>Weather Station Name</b><br />Last Updated 6 minutes ago.');           
-    }
-}
+// Get data from BC_Wildfire_Active_Weather_Stations CSV
+fetch('../data/BC_Wildfire_Active_Weather_Stations.csv').then(response => response.text()).then(data => {
+    // Parse CSV data
+    const rows = data.trim().split('\n');
+    const titles = rows[0].split(',');
+    const stations = rows.slice(1).map(row => {
+        const data = row.split(',');
+        const station = {};
+        titles.forEach((title, index) => {
+            station[title.trim()] = data[index].trim();
+        });
+        return station;
+    });
+
+    // Create markers for each weather station
+    stations.forEach(station => {
+        const marker = L.marker([parseFloat(station['Y']), parseFloat(station['X'])])
+            .addTo(map)
+            .bindPopup(
+            '<b>Station ID: ' + station['WEATHER_STATIONS_ID'] + '</b><br>' +
+            '<b>Station Code: ' + station['STATION_CODE'] + '</b><br>' +
+            '<b>Station Name: ' + station['STATION_NAME'] + '</b><br>' +
+            '<b>Station Acronym: ' + station['STATION_ACRONYM'] + '</b><br>' +
+            '<b>Latitude: ' + station['LATITUDE'] + '</b><br>' +
+            '<b>Longitude: ' + station['LONGITUDE'] + '</b><br>' +
+            '<b>Elevation: ' + station['ELEVATION'] + '</b><br>' +
+            '<b>Install Date: ' + station['INSTALL_DATE'] + '</b><br>'
+            );
+            
+        // When marker is clicked on, change the variables in the left column to display information
+        marker.on('click', function() {
+            // Update HTML elements with marker's weather station information
+            document.getElementById('station-name').innerText = station['STATION_NAME'] + " - #" + station['WEATHER_STATIONS_ID'];
+            document.getElementById('latitude').innerText = 'Latitude: ' + station['LATITUDE'];
+            document.getElementById('longitude').innerText = 'Longitude: ' + station['LONGITUDE'];
+            document.getElementById('elevation').innerText = 'Elevation: ' + station['ELEVATION'] + "m";
+        });
+    });
+});
+
