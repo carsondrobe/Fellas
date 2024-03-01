cd18c220dcd9cc6b84b72c4c761bf9cfa0906713
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-02-29 18:14:57 -0800

    Refactor CSV download and update process to be more efficient

--- Site/bc_weather_station_dashboard/website/management/commands/csv_scrape.py
+++ Site/bc_weather_station_dashboard/website/management/commands/csv_scrape.py
@@ -1,30 +1,34 @@
 from django.core.management.base import BaseCommand
 import requests
-from pytz import timezone
+from django.utils import timezone
 from datetime import datetime
-from website.models import StationData  
-from website.models import WeatherStation
+from website.models import StationData,WeatherStation
 import pandas as pd
-import os
 from django.utils.timezone import make_aware
-
+import tempfile
 
 class Command(BaseCommand):
-    help = 'Download CSV data and update WeatherStation model'
+    help = 'Manually download current day CSV data and update StationData model to run use the command: python manage.py csv_scrape.'
 
-    def download_csv(self, url, filename):
+    def download_csv(self, temp_file, url) -> bool:
+        """Downloads a CSV file and writes it to a temporary file."""
         response = requests.get(url)
+        # Check if the request was successful
         if response.status_code == 200:
-            with open(filename, 'wb') as file:
-                file.write(response.content)
-            self.stdout.write(self.style.SUCCESS(f"Downloaded file: {filename}"))
+            temp_file.write(response.content)
+            self.stdout.write(self.style.SUCCESS(f"Downloaded file: {temp_file.name}"))
+            return True
         else:
-            print(f"Failed to download file. Today's data is unavailable right now. {url}")
+            self.stdout.write(self.style.ERROR(f"Failed to download file. Today's data is unavailable right now. {url}"))
+            return False
 
-    def update_model_with_csv(self, filename):
+    def update_model_with_csv(self, filename) -> None:
+        """Updates the StationData model with data from a CSV file."""
         data = pd.read_csv(filename)
         for index, row in data.iterrows():
+            # Convert the date time string to a datetime object
             naive_datetime = datetime.strptime(str(row['DATE_TIME']), "%Y%m%d%H")
+            # Make the datetime object timezone aware
             aware_datetime = make_aware(naive_datetime)
             row['DATE_TIME'] = aware_datetime
 
@@ -44,27 +48,25 @@ class Command(BaseCommand):
             # Set the station field of the StationData instance
             row_data['station'] = station
 
+            # Create a new StationData object or update the existing one
             StationData.objects.get_or_create(**row_data)
 
-    def handle(self, *args, **kwargs):
-        date = datetime.now(timezone('America/Vancouver'))
+    def handle(self, *args, **kwargs)-> None:
+        """Handles the command, calls the other methods."""
+        # Get the current date in the 'America/Vancouver' timezone
+        date = timezone.localtime(timezone.now())
 
         # Format the date as yyyy-mm-dd
         date_str = date.strftime('%Y-%m-%d')
             
-        # Create the URL and filename
+        # Create the URL
         url = f'https://www.for.gov.bc.ca/ftp/HPR/external/!publish/BCWS_DATA_MART/2024/{date_str}.csv'
-        filename = f'{date_str}.csv'
 
-        # Try to download the CSV
-        self.download_csv(url, filename)
-        self.update_model_with_csv(filename)
-
-        try:
-            self.update_model_with_csv(filename)
-            self.stdout.write(self.style.SUCCESS('Data inserted into the model'))
-        finally:
-            # Delete the CSV file after model updated
-            if os.path.exists(filename):
-                os.remove(filename)
-                self.stdout.write(self.style.SUCCESS('File Delted Successfully'))
+        # Create a temporary file for the CSV being downloaded
+        with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+            # Try to download the CSV
+            if self.download_csv(temp_file, url):
+                temp_file.seek(0)  # Go to the start of the file
+                # Update the model with the data from the CSV
+                self.update_model_with_csv(temp_file.name)
+                self.stdout.write(self.style.SUCCESS(f'Most current data for {date_str} inserted into the StationData model'))
