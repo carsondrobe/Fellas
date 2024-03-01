35d7466c1d4ff6a8abac6ee6fb74b374c1599c5c
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-02-29 16:33:45 -0800

    Update CSV scraping and model updating logic

--- Site/bc_weather_station_dashboard/website/management/commands/csv_scrape.py
+++ Site/bc_weather_station_dashboard/website/management/commands/csv_scrape.py
@@ -1,10 +1,13 @@
 from django.core.management.base import BaseCommand
 import requests
-import time
 from pytz import timezone
-from datetime import datetime, timedelta
+from datetime import datetime
+from website.models import StationData  
 from website.models import WeatherStation
 import pandas as pd
+import os
+from django.utils.timezone import make_aware
+
 
 class Command(BaseCommand):
     help = 'Download CSV data and update WeatherStation model'
@@ -14,35 +17,54 @@ class Command(BaseCommand):
         if response.status_code == 200:
             with open(filename, 'wb') as file:
                 file.write(response.content)
-            print(f"Downloaded file: {filename}")
+            self.stdout.write(self.style.SUCCESS(f"Downloaded file: {filename}"))
         else:
             print(f"Failed to download file. Today's data is unavailable right now. {url}")
 
     def update_model_with_csv(self, filename):
         data = pd.read_csv(filename)
         for index, row in data.iterrows():
-            row['DATE_TIME'] = datetime.strptime(str(row['DATE_TIME']), "%Y%m%d%H")
-        
+            naive_datetime = datetime.strptime(str(row['DATE_TIME']), "%Y%m%d%H")
+            aware_datetime = make_aware(naive_datetime)
+            row['DATE_TIME'] = aware_datetime
+
             # Replace None or NaN values with a default value
             row_data = row.to_dict()
             for key, value in row_data.items():
                 if pd.isnull(value):
                     row_data[key] = 0
 
-            WeatherStation.objects.create(**row_data)
+            # Try to get the corresponding WeatherStation instance
+            try:
+                station = WeatherStation.objects.get(STATION_CODE=row_data['STATION_CODE'])
+            except WeatherStation.DoesNotExist:
+                # If the WeatherStation does not exist, skip to the next iteration
+                continue
+
+            # Set the station field of the StationData instance
+            row_data['station'] = station
+
+            StationData.objects.get_or_create(**row_data)
 
     def handle(self, *args, **kwargs):
         date = datetime.now(timezone('America/Vancouver'))
 
-        while True:
-            # Format the date as yyyy-mm-dd
-            date_str = date.strftime('%Y-%m-%d')
+        # Format the date as yyyy-mm-dd
+        date_str = date.strftime('%Y-%m-%d')
             
-            # Create the URL and filename
-            url = f'https://www.for.gov.bc.ca/ftp/HPR/external/!publish/BCWS_DATA_MART/2024/{date_str}.csv'
-            filename = f'{date_str}.csv'
-            
-            # Try to download the CSV
-            self.download_csv(url, filename)
+        # Create the URL and filename
+        url = f'https://www.for.gov.bc.ca/ftp/HPR/external/!publish/BCWS_DATA_MART/2024/{date_str}.csv'
+        filename = f'{date_str}.csv'
+
+        # Try to download the CSV
+        self.download_csv(url, filename)
+        self.update_model_with_csv(filename)
+
+        try:
             self.update_model_with_csv(filename)
-            time.sleep(14400)
+            self.stdout.write(self.style.SUCCESS('Data inserted into the model'))
+        finally:
+            # Delete the CSV file after model updated
+            if os.path.exists(filename):
+                os.remove(filename)
+                self.stdout.write(self.style.SUCCESS('File Delted Successfully'))
