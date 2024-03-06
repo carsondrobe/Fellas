642db70773e3e7006eed97373b678d2e7406e71d
carsondrobe <carsondrobe@gmail.com>
2024-03-05 19:51:55 -0800

    comment out login_required

--- Site/bc_weather_station_dashboard/website/views.py
+++ Site/bc_weather_station_dashboard/website/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required
+# @login_required
 def home(request):
     return render(request, "home.html", {})
 
