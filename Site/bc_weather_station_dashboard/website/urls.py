7d7724c68bff6b67266b558659a9e3ba00fd5573
carsondrobe <carsondrobe@gmail.com>
2024-03-05 19:53:24 -0800

    add comma

--- Site/bc_weather_station_dashboard/website/urls.py
+++ Site/bc_weather_station_dashboard/website/urls.py
@@ -22,7 +22,7 @@ from .views import weather_stations_data
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('login/', views.login, name='login')
+    path("login/", views.login, name="login"),
     path("weather_stations_data/", weather_stations_data, name="weather_stations_data"),
     path("submit_feedback/", views.submit_feedback, name="submit_feedback"),
 ]
