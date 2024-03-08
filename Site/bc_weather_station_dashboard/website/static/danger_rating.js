c38edfa1d18923f06e4aabdda3d55963473cb965
Davis Franklin <davis_franklin@outlook.com>
2024-03-07 19:11:35 -0800

    added js to make the arrow in the fire danger widget dynamic

--- Site/bc_weather_station_dashboard/website/static/danger_rating.js
+++ Site/bc_weather_station_dashboard/website/static/danger_rating.js
@@ -2,20 +2,20 @@
 var value = 1; // Example value
   
 // Get the arrow and arrow value elements
-const arrow = document.getElementById('arrowDR');
+const arrow = document.getElementById('arrow-fire-rating');
 
-if (value === 1){
-    value = 18;
-}else if (value === 2){
-    value = 39;    
-} else if (value === 3){
-    value = 59;    
-}else if (value === 4){
-    value = 80;
+if (value === 1 || value === 2){
+    value = 64;
+}else if (value === 3){
+    value = 168;    
+} else if (value === 4){
+    value = 272;    
+}else if (value === 5){
+    value = 376;
 }
 
 // Calculate the position of the arrow based on the value
-const position = value + '%';
+const position = value + 'px';
 
-// Set the top position of the arrow and display the value
+// Set the position of the arrow relative to the left
 arrow.style.left = position;
