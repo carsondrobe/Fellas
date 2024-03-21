a78708925ea0c90506d804acf5973d0362933c7a
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-20 22:24:50 -0700

    Got the html and logic of the precipitation card working.

---
+++ Site/bc_weather_station_dashboard/website/static/JavaScript/precipitation.js
@@ -0,0 +1,24 @@
+function rain() {
+    var cloud = document.querySelector('.cloud');
+    var e = document.createElement('div');
+    var left = Math.floor(Math.random() * 95);
+    var width = Math.random() * 3;
+    var height = Math.random() * 13;
+    var duration = Math.random() * 0.5;
+
+    e.classList.add('raindrop');
+    cloud.appendChild(e);
+    e.style.top = '20px';
+    e.style.left = 5 + left + 'px';;
+    e.style.width = width + 'px';;
+    e.style.height = height + 'px';;
+    e.style.animationDuration = 1 + duration + 's'
+    setTimeout(function() {
+        cloud.removeChild(e)
+    }, 2000)
+}
+
+setInterval(function() {
+    rain()
+    // Lower number means more raindrops
+}, 50);
