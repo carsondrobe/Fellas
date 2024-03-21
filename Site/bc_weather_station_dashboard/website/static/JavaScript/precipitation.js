8104c7735b3556d5d7b4633725ac6cbad68ed26f
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-21 10:06:30 -0700

    Added comments to precipitation.js file.

--- Site/bc_weather_station_dashboard/website/static/JavaScript/precipitation.js
+++ Site/bc_weather_station_dashboard/website/static/JavaScript/precipitation.js
@@ -1,11 +1,13 @@
+// Function to create raindrops
 function rain() {
+    // Get cloud and create html div for raindrops
     var cloud = document.querySelector('.cloud');
     var e = document.createElement('div');
     var left = Math.floor(Math.random() * 105);
     var width = Math.random() * 3;
     var height = Math.random() * 13;
     var duration = Math.random() * 0.5;
-
+    // Create raindrops
     e.classList.add('raindrop');
     cloud.appendChild(e);
     e.style.top = '20px';
