f56bfafc0190167bfae66d1cdf19f36aa606aa71
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-03-21 18:30:44 -0700

    Phone numbers follow E.164 standard

--- Site/bc_weather_station_dashboard/website/static/forms.js
+++ Site/bc_weather_station_dashboard/website/static/forms.js
@@ -1,40 +1,13 @@
-function validateLoginForm() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    if (email === '' || password === '') {
-        alert('Please fill in all fields');
-        return false;
-    }
-
-    if (email.indexOf('@') === -1) {
-        alert('Invalid email address');
-        return false;
-    }
-
-    if (password.length < 8) {
-        alert('Password must be at least 8 characters long');
-        return false;
-    }
-
-    return true;
-}
-
-function validateFeedbackForm() {
-    var feedback = document.getElementById('exampleFormControlTextarea1').value;
-    if (feedback.trim() === '') {
-        alert('Feedback is empty. Please enter your feedback.');
-        return false;
-    }
-    return true;
-}
-
 function validateRegisterForm() {
     const email = document.getElementById('registerEmail').value;
     const password = document.getElementById('registerPassword').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
+    const phoneNumber = iti.getNumber();
 
-    if (email === '' || password === '') {
+    // E.164 format regex
+    const e164Format = /^\+[1-9]\d{1,14}$/;
+
+    if (email === '' || password === '' || phoneNumber === '') {
         alert('Please fill in all fields');
         return false;
     }
@@ -51,6 +24,11 @@ function validateRegisterForm() {
 
     if (password !== confirmPassword) {
         alert('Passwords do not match');
+        return false;
+    }
+
+    if (!iti.isValidNumber() || !e164Format.test(phoneNumber)) {
+        alert("Please enter a valid phone number, ensuring it has a country code (e.g. +1)");
         return false;
     }
 
