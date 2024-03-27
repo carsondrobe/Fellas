2b95cb6b71a2421c813c09ee6e7b9037dfadc5f2
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-26 17:06:31 -0700

    Fixed sticky precip.

--- Site/bc_weather_station_dashboard/website/static/JavaScript/map.js
+++ Site/bc_weather_station_dashboard/website/static/JavaScript/map.js
@@ -109,7 +109,7 @@ function updateDataHTML(currentStationData) {
         progressBar.setValue(relativeHumidity);
     }
     // Update the HTML elements with the station's precipitation data
-    if (currentStationData.HOURLY_PRECIPITATION) {
+    if ('HOURLY_PRECIPITATION' in currentStationData) {
         document.getElementById('precipitation').innerHTML = currentStationData.HOURLY_PRECIPITATION + " mm";
     }
     // Update the HTML elements with the station's snow depth data
