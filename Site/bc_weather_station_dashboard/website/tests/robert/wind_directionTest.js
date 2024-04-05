f8523c612542c240597d512ccb8e02c473f19cba
carsondrobe <carsondrobe@gmail.com>
2024-04-05 13:01:12 -0700

    Bugfix with windirection test

--- Site/bc_weather_station_dashboard/website/tests/robert/wind_directionTest.js
+++ Site/bc_weather_station_dashboard/website/tests/robert/wind_directionTest.js
@@ -1,22 +1,22 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-const WindArrow = require('../../static/wind_direction.js');
+const WindArrow = require('../../static/WidgetJavaScript/wind_direction.js');
 
-QUnit.module('WindArrow', function(hooks) {
-    hooks.beforeEach(function() {
+QUnit.module('WindArrow', function (hooks) {
+    hooks.beforeEach(function () {
         // Simulate a DOM environment for testing
         const dom = new JSDOM(`<!DOCTYPE html><div id="wind-direction"></div>`);
         global.document = dom.window.document;
     });
 
-    QUnit.test('constructor', function(assert) {
+    QUnit.test('constructor', function (assert) {
         // Test that the constructor correctly adjusts degrees and calculates radians
         let windArrow = new WindArrow(90);
         assert.equal(windArrow.degrees, 0, 'Degrees are correctly adjusted');
         assert.equal(windArrow.radians, 0, 'Radians are correctly calculated');
     });
 
-    QUnit.test('createCanvas', function(assert) {
+    QUnit.test('createCanvas', function (assert) {
         // Test that the createCanvas method correctly sets the canvas width and height
         let windArrow = new WindArrow(90);
         let canvas = windArrow.createCanvas(350);
@@ -24,7 +24,7 @@ QUnit.module('WindArrow', function(hooks) {
         assert.equal(canvas.height, 350, 'Canvas height is correctly set');
     });
 
-    QUnit.test('appendCanvasToContainer', function(assert) {
+    QUnit.test('appendCanvasToContainer', function (assert) {
         // Test that the appendCanvasToContainer method correctly appends the canvas to the container
         let windArrow = new WindArrow(90);
         windArrow.createCanvas(350);
@@ -33,7 +33,7 @@ QUnit.module('WindArrow', function(hooks) {
         assert.equal(container.firstChild, windArrow.canvas, 'Canvas is appended to container');
     });
 
-    QUnit.test('calculateComponents', function(assert) {
+    QUnit.test('calculateComponents', function (assert) {
         // Test that the calculateComponents method correctly calculates the x and y components
         let windArrow = new WindArrow(90);
         windArrow.calculateComponents();
